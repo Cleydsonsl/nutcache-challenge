daa5afa68684f9086625c91094e18db28d8005b8
CleydsonSl <cley.souza007@gmail.com>
2021-06-14 20:16:56 -0300

    usando axios para api

--- src/components/EmployeesTable/index.tsx
+++ src/components/EmployeesTable/index.tsx
@@ -1,10 +1,11 @@
 import { useEffect } from "react";
+import { api } from "../../services/api";
 import { Container } from "./styles";
 
 export function EmployeesTable() {
   useEffect(() => {
-    fetch('http://localhost:3000/api/transactions')
-    .then(response => response.json()).then(data => console.log(data))
+    api.get('transactions')
+    .then(data => console.log(data))
   }, []);
 
   return (

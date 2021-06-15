56ebe8f6f4cff86205cf0ba2a626f53160993e73
CleydsonSl <cley.souza007@gmail.com>
2021-06-14 21:28:07 -0300

    estrutura do formulário

--- src/components/NewEmployeeModal/index.tsx
+++ src/components/NewEmployeeModal/index.tsx
@@ -1,4 +1,5 @@
 import Modal from 'react-modal';
+import { Container } from './styles';
 
 interface NewEmployeeModalProps {
   isOpen: boolean;
@@ -11,8 +12,40 @@ export function NewEmployeeModal({isOpen, onRequestClose}: NewEmployeeModalProps
     <Modal 
     isOpen={isOpen}
     onRequestClose={onRequestClose}
+    overlayClassName="react-modal-overlay"
+    className="react-modal-content"
     >
-      <h2>Cadastrar Funcionario</h2>
+      <Container>
+        <h2>Cadastrar Funcionario</h2>
+
+        <input 
+          placeholder="Nome" 
+        />
+        <input 
+          type="date" 
+        />
+        <select placeholder="Sexo">
+          <option value="Masculino">Masculino</option>
+          <option value="Feminino">Feminino</option>
+        </select>
+        <input 
+          placeholder="E-mail"
+        />
+        <input 
+          placeholder="CPF"
+        />
+        <input 
+          placeholder="Data de Inicio"
+        />
+        <select placeholder="Equipe">
+          <option value="Mobile">Mobile</option>
+          <option value="Front-End">Front-End</option>
+          <option value="Back-End">Front-End</option>
+        </select>
+        <button type="submit">
+          Cadastrar
+        </button>
+      </Container>
     </Modal>
   )
 }
